0fcc2318b1f32aafbb2e3e4e2377c4ddaccd685e
Ryan Park <ryan.developer99@gmail.com>
2021-06-07 18:56:53 +0900

    add: baseSeup for styling
    fetch API successfully and fix image size

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./index.css";
 
 function App() {
+  const url = "https://course-api.com/react-tours-project";
+
+  const [places, setPlaces] = useState([]);
+
+  const callBack = () => {
+    console.log("rendered");
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPlaces(data));
+  };
+
+  useEffect(callBack, []);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Our Tours</h1>
+      <article>
+        {places.map((place) => {
+          console.log(place);
+          const { id, name, info, image, price } = place;
+          return (
+            <div key={id}>
+              <img src={image} alt="" className="placeImg" />
+              <h4>{name}</h4>
+              <h4>{price}</h4>
+              <p>{info}</p>
+            </div>
+          );
+        })}
+      </article>
     </div>
   );
 }
